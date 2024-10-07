8de2f8952fbc117091acf226497a2bd7f1912caa
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-07 15:45:50 -0700

    Add custom tailwind fonts, themes, and grid

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "selector",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+      mono: [
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Liberation Mono",
+        "Courier New",
+        "monospace",
+      ],
+    },
+    fontSize: {
+      xs: "0.9rem",
+      sm: "1rem",
+      base: ["1.25rem", { lineHeight: "1.5rem" }],
+      lg: ["1.5rem", { lineHeight: "1.8rem" }],
+      xl: ["2rem", { lineHeight: "2.2rem" }],
+      "2xl": ["3.2rem", { lineHeight: "3.4rem" }],
+    },
+    extend: {
+      colors: {
+        black: "#1f1f1f",
+        white: "#fefcd9",
+      },
+      gridTemplateRows: {
+        game: "1fr min-content",
+      },
+    },
   },
   plugins: [],
 }
