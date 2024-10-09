02488fe42b71282f9faad5e2c263f7c415bb92d1
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-09 15:57:37 -0700

    Add storage_path support to Config

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,21 @@
-pub const DEFAULT_PORT: &str = "3000"; // This is stored as a string to match environment vars
+pub const DEFAULT_PORT: &str = "3000";
+pub const DEFAULT_STORAGE_PATH: &str = "_gamedata";
 
 /// The running API configuration
 pub struct Config {
     pub port: String,
+    pub storage_path: String,
 }
 
 impl Config {
     /// Creates a new config with values from environment variables, falls back to default values
     pub fn from_env() -> Self {
-        // Check for core networking such as PORT and HOST
-        let port = std::env::var("PORT")
-            .unwrap_or_else(|_| DEFAULT_PORT.to_string())
-            .parse()
-            .expect("PORT must be a number");
-        Config { port }
+        // Check for a non-default port
+        let port = std::env::var("PORT").unwrap_or_else(|_| DEFAULT_PORT.to_string());
+        // Check for a non-default storage path
+        let storage_path =
+            std::env::var("STORAGE_PATH").unwrap_or_else(|_| DEFAULT_STORAGE_PATH.to_string());
+        Config { port, storage_path }
     }
     /// Formats the host and port into an address for a TCPListener to bind to
     pub fn get_address(&self) -> String {
