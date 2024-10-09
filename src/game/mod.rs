a2a24887fa36095eb18d14f5ba946152bc8e39b2
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-09 13:53:31 -0700

    Add some basic game classes, add TODOs

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -4,9 +4,29 @@ use axum::{
 };
 
 pub async fn game_handler(Path(game_id): Path<String>) -> impl IntoResponse {
+    // TODO: Receive move made from player
+    // TODO: Get the Game from storage
+    // TODO: Validate and append the move
+    // TODO: Make another move as the computer
+    // TODO: Return move made to client
     format!("request for game {}", game_id)
 }
 
 pub async fn start_game(State(state): State<crate::AppState>) -> impl IntoResponse {
+    // TODO: Create a game, save it, return the ID
     format!("request for game {} with state", "no_id_yet")
+}
+
+struct Game {
+    moves: [Move; 9],
+}
+
+struct Move {
+    player: Player,
+    position: u32, // Maps to the cell that the move occurred, 1 - 9
+}
+
+pub enum Player {
+    Computer,
+    Player(u32),
 }
