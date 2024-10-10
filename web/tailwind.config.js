ebf5940851271c6c04b637fcdbbbe5cb5a0587ae
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-10 14:40:37 -0700

    Add new huge font size preset

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,35 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "selector",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    fontFamily: {
-      sans: ["Outfit", "sans-serif"],
-      mono: [
-        "ui-monospace",
-        "SFMono-Regular",
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        "Liberation Mono",
-        "Courier New",
-        "monospace",
-      ],
+    darkMode: "selector",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        fontFamily: {
+            sans: ["Outfit", "sans-serif"],
+            mono: [
+                "ui-monospace",
+                "SFMono-Regular",
+                "Menlo",
+                "Monaco",
+                "Consolas",
+                "Liberation Mono",
+                "Courier New",
+                "monospace",
+            ],
+        },
+        fontSize: {
+            xs: "0.9rem",
+            sm: "1rem",
+            base: ["1.25rem", { lineHeight: "1.5rem" }],
+            lg: ["1.5rem", { lineHeight: "1.8rem" }],
+            xl: ["2rem", { lineHeight: "2.2rem" }],
+            "2xl": ["3.2rem", { lineHeight: "3.4rem" }],
+            huge: ["9.5rem", { lineHeight: "9.7rem" }],
+        },
+        extend: {
+            colors: {
+                black: "#1f1f1f",
+                white: "#fefcd9",
+            },
+        },
     },
-    fontSize: {
-      xs: "0.9rem",
-      sm: "1rem",
-      base: ["1.25rem", { lineHeight: "1.5rem" }],
-      lg: ["1.5rem", { lineHeight: "1.8rem" }],
-      xl: ["2rem", { lineHeight: "2.2rem" }],
-      "2xl": ["3.2rem", { lineHeight: "3.4rem" }],
-    },
-    extend: {
-      colors: {
-        black: "#1f1f1f",
-        white: "#fefcd9",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 }
