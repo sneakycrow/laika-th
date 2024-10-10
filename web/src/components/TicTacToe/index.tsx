c7f7db8f865535f84879a5f96f958b7947e75ae3
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-09 17:23:25 -0700

    Connect start game frontend to new start game backend route handler

--- web/src/components/TicTacToe/index.tsx
+++ web/src/components/TicTacToe/index.tsx
@@ -6,100 +6,130 @@ import GameBoard from "./GameBoard"
 const CELLS_NUMBER = 9
 
 export enum Player {
-  Computer,
-  User,
+    Computer,
+    User,
 }
 
 export const renderPlayerToString = (player: Player | undefined): string => {
-  switch (player) {
-    case Player.Computer: {
-      return "o"
+    switch (player) {
+        case Player.Computer: {
+            return "o"
+        }
+        case Player.User: {
+            return "x"
+        }
+        case undefined:
+        default: {
+            return ""
+        }
     }
-    case Player.User: {
-      return "x"
-    }
-    case undefined:
-    default: {
-      return ""
-    }
-  }
 }
 
 export type Move = {
-  player: Player
-  cell: number // Should be a number between 1 and 9
+    player: Player
+    cell: number // Should be a number between 1 and 9
 }
 
 const TicTacToe = () => {
-  // The ID of the current game being played
-  const [gameID, setGameID] = useState("")
-  const [moves, setMoves] = useState<Move[]>([])
-  const [isDetailsShown, setShowDetails] = useState(false)
-  // Sends a request to the backend to start a game and updates the state appropriately
-  const startGame = () => {
-    // TODO: Send request to API which is expected to give an ID of the game
-    setGameID("some_id")
-  }
-  // A list of moves than have been made
-  const makePlayerMove = (cell: number) => {
-    // If a game is new, start a new game, then make the move
-    if (!gameID) {
-      startGame()
+    // The ID of the current game being played
+    const [gameID, setGameID] = useState("")
+    const [moves, setMoves] = useState<Move[]>([])
+    const [isDetailsShown, setShowDetails] = useState(false)
+    const [isPlayersTurn, setIsPlayersTurn] = useState(true)
+    // Sends a request to the backend to start a game and updates the state appropriately
+    const startGame = async () => {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/game`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                player_id: "some_id",
+            }),
+        })
+        if (!res.ok) {
+            throw "Bad response from API"
+        }
+        // The response is expected to contain a game ID we can reference in future updates
+        const json = await res.json()
+        if (!json.id) {
+            // Somethings gone wrong, throw an error for now
+            throw "No game ID in response"
+        }
+        setGameID(json.id)
     }
-    // Make sure the cell they're targeting is empty
-    if (moves.find((move) => move.cell === cell)) {
-      return
+    // A list of moves than have been made
+    const makePlayerMove = async (cell: number) => {
+        // If a game is new, start a new game, then make the move
+        if (!gameID) {
+            await startGame()
+        }
+        // Make sure the cell they're targeting is empty
+        if (moves.find((move) => move.cell === cell)) {
+            return
+        }
+        // Finished validating, start making move
+        // Update players turn first so we can allow the computer to make a move
+        setIsPlayersTurn(false)
+        // Moves made client side should always be the player
+        // Add the new move to the list of moves
+        setMoves([...moves, { player: Player.User, cell }])
+        // TODO: Send our move to the computer so they can respond
+        // TODO: Push the computers move to our moves list
+        // Allow the player to make their next move
+        setIsPlayersTurn(true)
     }
-    // Moves made client side should always be the player
-    // Add the new move to the list of moves
-    setMoves([...moves, { player: Player.User, cell }])
-  }
-  // Resets the game to it's beginning state
-  const resetGame = () => {
-    setMoves([])
-    startGame()
-  }
-  // Toggles the game stats being displayed
-  const toggleStats = () => {
-    setShowDetails(!isDetailsShown)
-  }
+    // Resets the game to it's beginning state
+    const resetGame = () => {
+        setMoves([])
+        startGame()
+    }
+    // Toggles the game stats being displayed
+    const toggleStats = () => {
+        setShowDetails(!isDetailsShown)
+    }
 
-  return (
-    <section className="grid grid-cols-2 gap-20 items-center justify-center max-w-screen-lg w-full">
-      <GameBoard cells={CELLS_NUMBER} onCellClick={makePlayerMove} moves={moves} />
-      <aside className="h-full flex flex-col items-start justify-start min-w-max gap-4 col-start-2">
-        <div>
-          <p className="text-2xl ">
-            You -{" "}
-            <span className="font-semibold">
-              {renderPlayerToString(Player.User)}
-            </span>
-          </p>
-          <p className="text-2xl ">
-            Computer -{" "}
-            <span className="font-semibold">
-              {renderPlayerToString(Player.Computer)}
-            </span>
-          </p>
-        </div>
-        {gameID ? (
-          <>
-            <Button onClick={resetGame}>Start Over</Button>
-            <button className="underline" onClick={toggleStats}>
-              {isDetailsShown ? "Hide" : "Show"} Details
-            </button>
-          </>
-        ) : (
-          <p>Select any square to start</p>
-        )}
-        {gameID && isDetailsShown && (
-          <p className="text-xs">
-            Game ID: <span className="underline text-base">{gameID}</span>
-          </p>
-        )}
-      </aside>
-    </section>
-  )
+    return (
+        <section className="grid grid-cols-2 gap-20 items-center justify-center max-w-screen-lg w-full">
+            <GameBoard
+                cells={CELLS_NUMBER}
+                onCellClick={makePlayerMove}
+                moves={moves}
+            />
+            <aside className="h-full flex flex-col items-start justify-start min-w-max gap-4 col-start-2">
+                <div>
+                    <p className="text-2xl ">
+                        You -{" "}
+                        <span className="font-semibold">
+                            {renderPlayerToString(Player.User)}
+                        </span>
+                    </p>
+                    <p className="text-2xl ">
+                        Computer -{" "}
+                        <span className="font-semibold">
+                            {renderPlayerToString(Player.Computer)}
+                        </span>
+                    </p>
+                </div>
+                {gameID ? (
+                    <>
+                        <Button onClick={resetGame}>Start Over</Button>
+                        <button className="underline" onClick={toggleStats}>
+                            {isDetailsShown ? "Hide" : "Show"} Details
+                        </button>
+                    </>
+                ) : (
+                    <p>Select any square to start</p>
+                )}
+                {gameID && isDetailsShown && (
+                    <p className="text-xs">
+                        Game ID:{" "}
+                        <span className="underline text-base">{gameID}</span>
+                    </p>
+                )}
+            </aside>
+        </section>
+    )
 }
 
 export default TicTacToe
